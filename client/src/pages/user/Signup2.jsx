69413e5a32865bef747e06ee21226b9e0fcc715f
Abhinand <abhin4nd6@gmail.com>
2024-04-30 15:28:11 +0530

    resolved some errors in user side

--- client/src/pages/user/Signup2.jsx
+++ client/src/pages/user/Signup2.jsx
@@ -1,6 +1,6 @@
 import { useFormik, Form, Field, Formik } from 'formik'
 import * as Yup from 'yup'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import { useSelector } from 'react-redux'
